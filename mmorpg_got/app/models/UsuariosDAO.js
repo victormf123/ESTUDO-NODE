270597e09078bfa351be258a9e10778943486e52
Matheus Freitas <matheus.freitas@ibti.net.br>
2020-04-16 12:07:30 -0300

    importando crypto e configurando a senha

--- mmorpg_got/app/models/UsuariosDAO.js
+++ mmorpg_got/app/models/UsuariosDAO.js
@@ -1,11 +1,15 @@
+/** importar o módulo do crypto */
+var crypto = require('crypto')
 function UsuariosDAO(connection) {
   this.__connection = connection();
 }
 
 UsuariosDAO.prototype.inserirUsuario = function (usuario) {
-  console.log(usuario);
+  //console.log(usuario);
   this.__connection.open(function (err, mongoclient) {
     mongoclient.collection('usuarios', function (err, collection) {
+      var senha_criptografada = crypto.createHash('md5').update(usuario.senha).digest("hex");
+      usuario.senha = senha_criptografada;
       collection.insert(usuario);
 
       mongoclient.close();
@@ -14,9 +18,10 @@ UsuariosDAO.prototype.inserirUsuario = function (usuario) {
 };
 
 UsuariosDAO.prototype.autenticar = function (usuario, req, res) {
-  // console.log(usuario);
   this.__connection.open(function (err, mongoclient) {
     mongoclient.collection('usuarios', function (err, collection) {
+      var senha_criptografada = crypto.createHash('md5').update(usuario.senha).digest("hex");
+      usuario.senha = senha_criptografada;
       collection.find(usuario).toArray(function (err, result) {
         if (result[0] != undefined) {
           req.session.autorizado = true;
